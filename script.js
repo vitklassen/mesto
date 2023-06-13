895566376df46361ea4ff518108643bd5ba62081
Viktor Klassen <vit.klassen@yandex.ru>
2023-06-13 12:53:05 +0500

    add popUp

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-const editButton = document.querySelector('.profile__edit-button');
+let editButton = document.querySelector(".profile__edit-button");
+let popupClose = document.querySelector('.popUp__close-button');
+let popup = document.querySelector(".popUp");
+let likeButton = document.querySelectorAll(".elements__like-button");
+
+function openPopup (e){
+    e.preventDefault();
+    popup.classList.add("popup_opened");
+}
+function closePopup (){
+    popup.classList.remove("popup_opened");
+}
+function addLike (){
+    for (var j = 0; j < likeButton.length; j++) {
+        likeButton[j].classList.add("elements__like-button_active");
+    }
+}
+for (var i = 0; i < likeButton.length; i++) {
+    likeButton[i].addEventListener('click', addLike);
+}
+editButton.addEventListener('click', openPopup);
+popupClose.addEventListener('click', closePopup);
