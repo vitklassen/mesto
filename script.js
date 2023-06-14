f3f735bacfddea338af7b367cace3d7f7722c7c7
Viktor Klassen <vit.klassen@yandex.ru>
2023-06-14 08:05:19 +0500

    add-all-blocks

--- script.js
+++ script.js
@@ -1,22 +1,32 @@
-let editButton = document.querySelector(".profile__edit-button");
-let popupClose = document.querySelector('.popUp__close-button');
-let popup = document.querySelector(".popUp");
-let likeButton = document.querySelectorAll(".elements__like-button");
+let root = document.querySelector('.root');
+let editBtn = root.querySelector('.profile__edit-button');
+let popup = root.querySelector('.popup');
+let like = root.querySelector('.like-root');
+let formElement = popup.querySelector('.popup__form');
+let nameInput = formElement.querySelector('.popup__input_name_firstname');
+let jobInput = formElement.querySelector('.popup__input_name_user-info');
 
-function openPopup (e){
-    e.preventDefault();
-    popup.classList.add("popup_opened");
+function openPopup(popup) {
+    popup.classList.add('popup_opened');
 }
-function closePopup (){
-    popup.classList.remove("popup_opened");
-}
-function addLike (){
-    for (var j = 0; j < likeButton.length; j++) {
-        likeButton[j].classList.add("elements__like-button_active");
+function closePopup(evt) {
+    const closeBtn = evt.target;
+    if (closeBtn.classList.contains('popup__close-button')) {
+        closeBtn.closest('.popup').classList.remove('popup_opened');
     }
 }
-for (var i = 0; i < likeButton.length; i++) {
-    likeButton[i].addEventListener('click', addLike);
+function addLike(evt) {
+    const likeButton = evt.target;
+    if (likeButton.classList.contains('elements__like-button')) {
+        likeButton.classList.add('elements__like-button_active');
+    }
 }
-editButton.addEventListener('click', openPopup);
-popupClose.addEventListener('click', closePopup);
+function setPopupInputValue() {
+
+}
+
+editBtn.addEventListener('click', function () {
+    openPopup(popup);
+});
+root.addEventListener('click', closePopup);
+like.addEventListener('click', addLike);
