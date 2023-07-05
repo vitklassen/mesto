e7f25faac51b6e74aec6cb39121fc92f00fb63c1
Viktor Klassen <vit.klassen@yandex.ru>
2023-07-05 21:53:40 +0500

    change js

--- scripts/script.js
+++ scripts/script.js
@@ -7,6 +7,33 @@ let jobInput = formElement.querySelector('.popup__input_name_user-info');
 let nameUser = root.querySelector('.profile__name');
 let jobUser = root.querySelector('.profile__job');
 
+const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ]; 
+
 function openPopup(modal) {
     modal.classList.add('popup_opened');
 }
