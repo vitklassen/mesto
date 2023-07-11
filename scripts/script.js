ef885c1f03664438b89d77fb19be2e2d153694e0
Viktor Klassen <vit.klassen@yandex.ru>
2023-07-11 10:41:13 +0500

    fix-js

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,21 @@
-let root = document.querySelector('.page');
-let editBtn = root.querySelector('.profile__edit-button');
-let addBtn = root.querySelector('.profile__add-button');
-let editPopup = root.querySelector('.popup_type_edit');
-let formEditElement = editPopup.querySelector('.popup__form');
-let addPopup = root.querySelector('.popup_type_add');
-let formAddElement = addPopup.querySelector('.popup__form');
-let nameInput = formEditElement.querySelector('.popup__input_name_firstname');
-let jobInput = formEditElement.querySelector('.popup__input_name_user-info');
-let nameUser = root.querySelector('.profile__name');
-let jobUser = root.querySelector('.profile__job');
-let nameCardInput = formAddElement.querySelector('.popup__input_name_name');
-let linkCardInput = formAddElement.querySelector('.popup__input_name_link');
-let cardElements = root.querySelector('.elements');
-let popupCard = root.querySelector('.popup-card');
-let popupCardPhoto = popupCard.querySelector('.popup-card__photo');
-let popupCardTitle = popupCard.querySelector('.popup-card__title');
-let templateElement = root.querySelector('#template-elements__element').content.querySelector('.elements__element');
+const root = document.querySelector('.page');
+const editBtn = root.querySelector('.profile__edit-button');
+const addBtn = root.querySelector('.profile__add-button');
+const editPopup = root.querySelector('.popup_type_edit');
+const formEditElement = editPopup.querySelector('.popup__form');
+const addPopup = root.querySelector('.popup_type_add');
+const formAddElement = addPopup.querySelector('.popup__form');
+const nameInput = formEditElement.querySelector('.popup__input_name_firstname');
+const jobInput = formEditElement.querySelector('.popup__input_name_user-info');
+const nameUser = root.querySelector('.profile__name');
+const jobUser = root.querySelector('.profile__job');
+const nameCardInput = formAddElement.querySelector('.popup__input_name_name');
+const linkCardInput = formAddElement.querySelector('.popup__input_name_link');
+const cardElements = root.querySelector('.elements');
+const popupCard = root.querySelector('.popup-card');
+const popupCardPhoto = popupCard.querySelector('.popup-card__photo');
+const popupCardTitle = popupCard.querySelector('.popup-card__title');
+const templateElement = root.querySelector('#template-elements__element').content.querySelector('.elements__element');
 const initialCards = [
     {
       name: 'Архыз',
@@ -79,7 +79,7 @@ function setNodeTextValue() {
     jobUser.textContent = jobInput.value;
 }
 function setDataNewCard() {
-  let Card = [
+  const Card = [
     {
       name: '',
       link: ''
@@ -96,7 +96,7 @@ function formSubmitHandler(evt) {
 }
 function sendingCardData (evt) {
   evt.preventDefault();
-  let initialCard = setDataNewCard();
+  const initialCard = setDataNewCard();
   renderCards(initialCard, cardElements);
   closePopup(addPopup);
 }
@@ -107,6 +107,7 @@ function createCards (input) {
   const btnDelElement = elElement.querySelector('.elements__delete-button');
   const likeBtn = elElement.querySelector('.elements__like-button');
   photoElement.src = input.link;
+  photoElement.alt = 'Красивое место';
   titleElement.textContent = input.name;
   btnDelElement.addEventListener('click', function() {
     elElement.remove();
@@ -123,6 +124,7 @@ function createCards (input) {
   
   photoElement.addEventListener('click', function() {
     popupCardPhoto.src = input.link;
+    popupCardPhoto.alt = 'Красивое место';
     popupCardTitle.textContent = input.name;
     openPopup(popupCard);
   });
