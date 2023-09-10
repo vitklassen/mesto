74942e83e4471814e0b758687c876fae10a9f2c6
Viktor Klassen <vit.klassen@yandex.ru>
2023-09-10 21:42:16 +0500

    add card

---
+++ src/components/Api.js
@@ -0,0 +1,72 @@
+export default class Api {
+    constructor({url, header, token}) {
+        this._url = url;
+        this._header = header;
+        this._token = token;
+    }   
+    getAllCards() {
+        return fetch(`${this._url}cards`, {
+            method: 'GET',
+            headers: {
+                authorization: this._token,
+                'Content-Type': this._header
+            }
+        })
+        .then((response) => {
+            if(response.ok) {
+                return response.json();
+            }
+            throw new Error('Что-то пошло не так...');
+        })
+        .catch((error) => {
+            console.log(error.status, error.statusText);
+        })
+    }
+
+    getUserInfo() {
+        return fetch(`${this._url}users/me`, {
+            method: 'GET',
+            headers: {
+                authorization: this._token,
+                'Content-Type': this._header
+            }
+        })
+        .then((response) => {
+            if(response.ok) {
+                return response.json();
+            }
+            throw new Error('Что-то пошло не так...');
+        })
+        .catch((error) => {
+            console.log(error.status, error.statusText);
+        })
+    }
+
+    setUserInfo(userInfo) {
+        fetch(`${this._url}users/me`, {
+            method: 'PATCH',
+            headers: {
+                authorization: this._token,
+                'Content-Type': this._header
+            },
+            body: JSON.stringify({
+                name: userInfo.firstname,
+                about: userInfo.job
+            })
+        })
+    }
+
+    addNewCard(newCard) {
+        fetch(`${this._url}cards`, {
+            method: 'POST',
+            headers: {
+                authorization: this._token,
+                'Content-Type': this._header
+            }, 
+            body: JSON.stringify({
+                name: newCard.name,
+                link: newCard.link
+            })
+        })
+    }
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,15 @@
+//импорт требуемых компонентов
 import Section from "../components/Section.js";
 import Card from "../components/Card.js";
 import FormValidator from "../components/FormValidator.js";
 import PopupWithImage from "../components/PopupWithImage.js";
 import PopupWithForm from "../components/PopupWithForm.js";
 import UserInfo from "../components/UserInfo.js";
-import { initialCards, settingsOptions } from "../constants/data.js";
+import Api from "../components/Api.js";
+import { settingsOptions, cardSettings } from "../constants/data.js";
 import "./index.css";
 
+//константы
 const buttonOpenEditProfilePopup = document.querySelector(
   ".profile__edit-button"
 );
@@ -17,9 +20,30 @@ const popupAddCard = document.querySelector(".popup_type_add");
 const formAddElement = popupAddCard.querySelector(".popup__form");
 const nameInput = document.querySelector(".popup__input_name_firstname");
 const jobInput = document.querySelector(".popup__input_name_job");
+const profileName = document.querySelector(".profile__name");
+const profileJob = document.querySelector(".profile__job");
+const profileAvatar = document.querySelector(".profile__avatar");
+//экземпляр класса Api
+const newApi = new Api(cardSettings);
 
+//загрузка карточек с сервера
+newApi.getAllCards()
+.then((cards) => {
+  newSection.render(cards);;
+})
+
+//загрузка информации о пользователе
+newApi.getUserInfo()
+.then((userInfoObject) => {
+  profileName.textContent = userInfoObject.name;
+  profileJob.textContent = userInfoObject.about;
+  profileAvatar.src = userInfoObject.avatar;
+})
+
+//экземпляр класса UserInfo
 const userInfo = new UserInfo(".profile__name", ".profile__job");
 
+//экземпляр класса Section
 const newSection = new Section(
   {
     renderer: (item) => {
@@ -30,27 +54,33 @@ const newSection = new Section(
   ".elements"
 );
 
+//экземпляры класса FormValidator и валидация полей формы
 const newValidityEditForm = new FormValidator(settingsOptions, formEditElement);
 newValidityEditForm.enableValidation();
 const newValidityAddForm = new FormValidator(settingsOptions, formAddElement);
 newValidityAddForm.enableValidation();
 
+//экземпляр класса PopupWithForm для формы попапа редактирования профиля
 const popupWithEditForm = new PopupWithForm({
   popupSelector: ".popup_type_edit",
   handleFormSubmit: (formData) => {
     userInfo.setUserInfo(formData);
+    newApi.setUserInfo(formData);
   },
   protectFromBadData: () => {
     return;
   },
 });
 
+//слушатели событий для попапа редактирования профиля
 popupWithEditForm.setEventListeners();
 
+//экземпляр класса PopupWithForm для формы попапа создания карточки
 const popupWithAddForm = new PopupWithForm({
   popupSelector: ".popup_type_add",
   handleFormSubmit: (formData) => {
     const newCard = createCard(formData);
+    newApi.addNewCard(formData);
     newSection.addItem(newCard.createCard());
     newValidityAddForm.disableSubmitButton();
   },
@@ -58,11 +88,15 @@ const popupWithAddForm = new PopupWithForm({
     newValidityAddForm.disableSubmitButton();
   },
 });
+
+//слушатели событий для попапа создания карточки
 popupWithAddForm.setEventListeners();
 
+//экземпляр класса popupWithImage
 const popupWithImage = new PopupWithImage(".popup_type_card");
 popupWithImage.setEventListeners();
 
+//слушатель клика для кнопки редактирования профиля
 buttonOpenEditProfilePopup.addEventListener("click", function () {
   const currentUserInfo = userInfo.getUserInfo();
   nameInput.value = currentUserInfo.userName;
@@ -70,10 +104,12 @@ buttonOpenEditProfilePopup.addEventListener("click", function () {
   popupWithEditForm.open();
 });
 
+//слушатель клика для кнопки создания карточки
 buttonOpenAddCardPopup.addEventListener("click", function () {
   popupWithAddForm.open();
 });
 
+//функция для создания новой карточки
 function createCard(data) {
   const card = new Card(data, {
     templateSelector: "template-elements__element",
@@ -83,5 +119,3 @@ function createCard(data) {
   });
   return card;
 }
-
-newSection.render(initialCards);
