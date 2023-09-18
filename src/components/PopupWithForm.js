e22404317964725aad1f5faa681434388b300262
Viktor Klassen <vit.klassen@yandex.ru>
2023-09-18 09:48:10 +0500

    add Load

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -5,6 +5,7 @@ export default class PopupWithForm extends Popup {
     this._popup = super._generate();
     this._form = this._popup.querySelector(".popup__form");
     this._inputList = this._form.querySelectorAll(".popup__input");
+    this._saveButton = this._popup.querySelector(".popup__save-button");
     this._handleFormSubmit = handleFormSubmit;
     this._protectFromBadData = protectFromBadData;
   }
@@ -16,10 +17,20 @@ export default class PopupWithForm extends Popup {
 
     return this._formValues;
   }
+  _isLoading(param) {
+    if(param) {
+      this._saveButton.textContent = "Сохранение...";
+    }
+    else {
+      this._saveButton.textContent = "Сохранить";
+    }
+  }
   setEventListeners() {
     this._form.addEventListener("submit", () => {
+      this._isLoading(true);
       this._handleFormSubmit(this._getInputValues());
       this.close();
+      this._isLoading(false);
     });
     super.setEventListeners();
   }
